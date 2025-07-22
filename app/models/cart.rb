cf351a17b82183ee5f4daebaf5f6abda3e5f8d05
tabata lisboa <tabatalisboa@pm.me>
2025-07-22 18:59:35 -0300

    add:Associations

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,4 +1,7 @@
 class Cart < ApplicationRecord
+  has_many :cart_products, dependent: :destroy
+  has_many :products, through: :cart_products
+
   validates_numericality_of :total_price, greater_than_or_equal_to: 0
 
   # TODO: lógica para marcar o carrinho como abandonado e remover se abandonado
