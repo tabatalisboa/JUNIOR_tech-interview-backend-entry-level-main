fb362b00f66b109c46841bb3328f94314ca102eb
tabata lisboa <tabatalisboa@pm.me>
2025-07-24 22:57:39 -0300

    add:specs

--- spec/requests/carts_spec.rb
+++ spec/requests/carts_spec.rb
@@ -1,8 +1,18 @@
 require 'rails_helper'
 
-RSpec.describe "/carts", type: :request do
-  # pending "TODO: Escreva os testes de comportamento do controller de carrinho necessários para cobrir a sua implmentação #{__FILE__}"
-  describe "POST /add_items" do
+RSpec.describe "/cart", type: :request do  
+  let(:product) { Product.create(name: "Test Product", price: 10.0) }
+  describe "GET /cart" do
+    let(:cart) { Cart.create }
+    let!(:cart_item) { CartItem.create(cart: cart, product: product, quantity: 2) }
+
+    it "returns the current cart" do
+      get "/cart", as: :json
+      expect(response).to have_http_status(:ok)
+    end
+  end
+  
+  describe "POST /add_item" do
     let(:cart) { Cart.create }
     let(:product) { Product.create(name: "Test Product", price: 10.0) }
     let!(:cart_item) { CartItem.create(cart: cart, product: product, quantity: 1) }  
@@ -16,6 +26,19 @@ RSpec.describe "/carts", type: :request do
       it 'updates the quantity of the existing item in the cart' do
         expect { subject }.to change { cart_item.reload.quantity }.by(2)
       end
+    end
+  end
+  
+  describe "DELETE /cart/:product_id" do
+    let(:cart) { Cart.create }
+    let!(:cart_item) { CartItem.create(cart: cart, product: product, quantity: 1) }
+
+    subject do
+      delete "/cart/#{product.id}", as: :json
+    end
+
+    it "removes the item from the cart" do
+      expect { subject }.to change { cart.cart_items.count }.by(-1)
     end
   end
 end
